49ff6bf5e665aeddad2dd676ad1f8cd9c977cb91
Austin Aigbe <eshikafe@gmail.com>
2022-08-20 11:18:33 +0100

    Add reference information

--- src/ecma/mod.rs
+++ src/ecma/mod.rs
@@ -1,2 +1,7 @@
+// Reference document: https://www.ecma-international.org/publications-and-standards/standards/ecma-392/
+// The ECMA 392 Standard specifies a medium access control (MAC) sub-layer and 
+// a physical (PHY) layer for personal/portable cognitive wireless networks operating in TV bands. 
+
+
 pub mod frame;
 pub use frame::*;
